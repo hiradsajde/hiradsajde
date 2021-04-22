9683c79c737d988110b0fedc82317f85bc4372e4
hiradsajde <hiradsajde@localhost.localdomain>
2021-04-22 17:00:13 +0430

    inital commit

--- components/works.js
+++ components/works.js
@@ -4,20 +4,20 @@ const Card = () => {
         <div className={styles.works}>
             <div className={styles.card}>
                 <img src="/images/logo.png"/>
-                <p className={styles.place}>iTLog.ir <span>CEO</span><br/>
-                <div className={styles.time}>2021-2020</div>
+                <p className={styles.place}>Freelance <span>junior</span><br/>
+                <div className={styles.time}>2018-2019</div>
                 </p>
             </div>
             <div className={styles.card}>
                 <img src="/images/logo.png"/>
                 <p className={styles.place}>iTLog.ir <span>CEO</span><br/>
-                <div className={styles.time}>2021-2020</div>
+                <div className={styles.time}>2019-2021</div>
                 </p>
             </div>
             <div className={styles.card}>
                 <img src="/images/logo.png"/>
-                <p className={styles.place}>iTLog.ir <span>CEO</span><br/>
-                <div className={styles.time}>2021-2020</div>
+                <p className={styles.place}>Biamoz.io <span>CEO</span><br/>
+                <div className={styles.time}>2021-2022</div>
                 </p>
             </div>
         </div>
